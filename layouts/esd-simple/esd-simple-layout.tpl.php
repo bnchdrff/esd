0502eb13fea184d0bb94d0bb963ecd4bbcb76b5a
Douglas G Reith <doug@theworkdept.com>
2013-04-12 17:25:17 -0400

    refs #2795 - 7: blog block, search box, learn/take-action pages.

--- layouts/esd-simple/esd-simple-layout.tpl.php
+++ layouts/esd-simple/esd-simple-layout.tpl.php
@@ -27,7 +27,6 @@
 
     <div class="l-content" role="main">
       <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if ($title): ?>
