8f3dd980088bfddec95b3208651bf3b1b5d26992
Benjamin Chodoroff <ben@theworkdept.com>
2013-04-17 16:53:47 -0400

    refs #2795: region and layout refactor

--- layouts/esd-home/esd-home.tpl.php
+++ layouts/esd-home/esd-home.tpl.php
@@ -1,56 +1,64 @@
-<div class="l-page">
-  <div class="l-main">
-    <header class="l-header" role="banner">
-      <div class="l-branding">
-        <?php if ($logo): ?>
-          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+<header class="header">
+  <!-- intentionally left empty for progressive enhancement elements -->
+</header>
+
+<div class="content">
+
+<aside class="content-left">
+  <div class="branding" role="banner">
+    <?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+    <?php endif; ?>
+
+    <?php if ($site_name || $site_slogan): ?>
+      <hgroup>
+        <?php if ($site_name): ?>
+          <h1 class="site-name">
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+          </h1>
         <?php endif; ?>
 
-        <?php if ($site_name || $site_slogan): ?>
-          <hgroup>
-            <?php if ($site_name): ?>
-              <h1 class="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </h1>
-            <?php endif; ?>
-
-            <?php if ($site_slogan): ?>
-              <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
-            <?php endif; ?>
-          </hgroup>
+        <?php if ($site_slogan): ?>
+          <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
         <?php endif; ?>
-      </div>
-
-      <?php print render($page['header']); ?>
-      <?php print render($page['navigation']); ?>
-    </header>
-
-    <div class="l-content" role="main">
-      <?php print render($page['highlighted']); ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-    </div>
-
-    <?php print render($page['sidebar']); ?>
+      </hgroup>
+    <?php endif; ?>
+    <?php print render($page['branding']); ?>
   </div>
+</aside>
 
-  <div class="l-footer-wrapper">
-    <footer class="l-footer" role="contentinfo">
-      <?php print render($page['footer_first']); ?>
-      <?php print render($page['footer_second']); ?>
-      <?php print render($page['footer_third']); ?>
-    </footer>
+<section class="content-right">
+  <?php print render($page['pre_content']); ?>
+  <div class="l-content" role="main">
+    <a id="main-content"></a>
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?>
+      <h1><?php print $title; ?></h1>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>
+    <?php print $messages; ?>
+    <?php print render($tabs); ?>
+    <?php print render($page['help']); ?>
+    <?php if ($action_links): ?>
+      <ul class="action-links"><?php print render($action_links); ?></ul>
+    <?php endif; ?>
+    <?php print render($page['content']); ?>
+    <?php print $feed_icons; ?>
   </div>
+  <?php print render($page['post_content']); ?>
+</section>
+
+</div>
+
+<div class="footer">
+  <footer class="footer-left" role="contentinfo">
+    <?php print render($page['footer_first']); ?>
+  </footer>
+  <footer class="footer-middle">
+    <?php print render($page['footer_second']); ?>
+  </footer>
+  <footer class="footer-right">
+    <?php print render($page['footer_third']); ?>
+    <?php print render($page['footer_fourth']); ?>
+  </footer>
 </div>
